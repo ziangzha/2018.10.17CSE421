17fb646c47551c147f9dc32d1a239fc005ed112e
Floyd <36140953+ziangzha@users.noreply.github.com>
2018-10-18 21:24:39 -0400

    Update thread.h

--- thread.h
+++ thread.h
@@ -138,6 +138,6 @@ void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
 
-void timer_sleeping_thread(struct thread *t, void *aux UNUSED)
+void timer_sleeping_thread(struct thread *t, void *aux UNUSED);
 
 #endif /* threads/thread.h */
