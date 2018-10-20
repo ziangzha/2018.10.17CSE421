53a1ca748fda8fd1130f2c5f5aacfbba873ae12b
Floyd <36140953+ziangzha@users.noreply.github.com>
2018-10-20 15:05:36 -0400

    Update thread.c

--- thread.c
+++ thread.c
@@ -205,7 +205,7 @@ thread_create (const char *name, int priority,
   /* Add to run queue. */
   thread_unblock (t);
    
-  if(thread_current()->priority < priority){
+  if(priority > thread_current()->priority){
      thread_yield();
   }
 
