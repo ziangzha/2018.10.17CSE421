0dcacd4a6b780cbb97e7e9f5df1e985dab685acd
YizhouSun <yizhousu@buffalo.edu>
2018-10-21 18:30:09 -0400

    part3

--- thread.h
+++ thread.h
@@ -89,7 +89,9 @@ struct thread
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
     struct list_elem allelem;           /* List element for all threads list. */
-
+    //add new thing recent_cpu and nice
+    double_Type recent_cpu;
+    int nice;
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
